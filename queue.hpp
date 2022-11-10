f2d5db24dcd37f9de1d9b8e57323007d13ccf825
Antonio Catanzaro <92448892+KoteiMadoshi@users.noreply.github.com>
2022-11-10 22:43:40 +0100

    Update queue.hpp
    Combined classes used for sending exceptions into a single class

--- queue.hpp
+++ queue.hpp
@@ -7,45 +7,37 @@ using namespace std;
 
 template < class value_type >
 class queue {
-	public:
-		virtual bool empty() const = 0;
-		virtual value_type read_front() const = 0;
-		virtual void push_back(const value_type&) = 0;
-		virtual void pop_front() = 0;
+public:
+	virtual bool empty() const = 0;
+	virtual value_type read_front() const = 0;
+	virtual void push_back(const value_type&) = 0;
+	virtual void pop_front() = 0;
 };
 
-class invalidpop {
+class invalidqueueoperator {
 public:
-	invalidpop() {
+	invalidqueueoperator() {
+		this->error = "Invalid operator!";
+	}
+
+	invalidqueueoperator invalidpop() {
 		this->error = "Queue empty. Impossible to pop an element.";
+		return *this;
 	}
 
-	void print_error() {
-		cout << error << endl;
-	}
-
-private:
-	string error;
-};
-
-class invalidpush {
-public:
-	invalidpush() {
+	invalidqueueoperator invalidpush() {
 		this->error = "Queue full. Impossible to push an element.";
+		return *this;
 	}
 
-	void print_error() {
-		cout << error << endl;
-	}
-
-private:
-	string error;
-};
-
-class invalidread {
-public:
-	invalidread() {
+	invalidqueueoperator invalidread() {
 		this->error = "Queue empty. Impossible to read an element.";
+		return *this;
+	}
+
+	invalidqueueoperator invalidassignment() {
+		this->error = "Invalid assignment. The maximum dimension of the left queue is lesser than the maximum dimension of the right one.";
+		return *this;
 	}
 
 	void print_error() {
@@ -55,5 +47,6 @@ public:
 private:
 	string error;
 };
+
 
 #endif // !QUEUE_HPP_INCLUDED
